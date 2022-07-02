87f5c7d21aa1b8717f9118c67edacee01465471a
ragor114 <69634980+ragor114@users.noreply.github.com>
2022-07-02 13:27:51 +0200

    updated versions in starter

--- app/src/main/java/de/ur/mi/android/demos/AudioPlayerActivity.java
+++ app/src/main/java/de/ur/mi/android/demos/AudioPlayerActivity.java
@@ -1,11 +1,12 @@
 package de.ur.mi.android.demos;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class AudioPlayerActivity extends AppCompatActivity {
+public class AudioPlayerActivity extends Activity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
--- app/src/main/java/de/ur/mi/android/demos/MainActivity.java
+++ app/src/main/java/de/ur/mi/android/demos/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private RecyclerView recyclerAudioList;
 
